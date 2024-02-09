75d605a4c6db108e4a583e1521649d916b0f177f
Basim <basimsiddique@yahoo.com>
2024-02-09 18:07:11 +0500

    first commit

---
+++ src/main/java/com/sunrise/sunriseapp/SunriseappApplication.java
@@ -0,0 +1,13 @@
+package com.sunrise.sunriseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunriseappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunriseappApplication.class, args);
+	}
+
+}
